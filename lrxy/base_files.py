55bcee7fab26cbe4701eb8c8403a2c323f5847e5
MatsubaraCoder <qdmahdiqd@gmail.com>
2024-08-24 20:58:16 +0330

    Creating base_file.py for for common tasks work with directories and files

---
+++ lrxy/base_files.py
@@ -0,0 +1,22 @@
+from typing import Union
+from pathlib import Path
+
+
+class BaseFile:
+    def __init__(self, path: Union[str, Path]) -> None:
+        if isinstance(path, str):
+            self.path = Path(path).expanduser()
+        elif isinstance(path, Path):
+            self.path = path.expanduser()
+        else:
+            raise ValueError(
+                "The path must be a string or a pathlib.Path object")
+
+    def _check_path_exists(self) -> bool:
+        return self.path.exists()
+
+    def _check_is_file(self) -> bool:
+        return self.path.is_file()
+
+    def _check_is_directory(self) -> bool:
+        return self.path.is_dir()
