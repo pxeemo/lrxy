d5f7aa53726124c22784c28c8fe7c2a2e7c5ceaf
MatsubaraCoder <qdmahdiqd@gmail.com>
2024-08-25 15:28:59 +0330

    Update

--- lrxy/formats/flac.py
+++ lrxy/formats/flac.py
@@ -8,7 +8,7 @@ from .audio import Audio
 
 class Flac(Audio):
     def __init__(self, path: Union[Path, str]):
-        super().__init__(Path(path).expanduser(),
+        super().__init__(path,
                          FLAC, ["artist", "title", "album"])
 
     def embed_lyric(self, lyric: str) -> None:
