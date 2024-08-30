cf1b486939eb86d7c83fda4d3a21de21ad1425ee
MatsubaraCoder <qdmahdiqd@gmail.com>
2024-08-30 19:27:37 +0330

    Update __init__.py

--- lrxy/formats/__init__.py
+++ lrxy/formats/__init__.py
@@ -1,6 +1,7 @@
 from .flac import Flac
 from .mp3 import Mp3
 from .mp4 import Mp4
+from .audio import SUPPORTRD_FORMATS
 
 
-__all__ = ["Flac", "Mp3", "Mp4"]
+__all__ = ["Flac", "Mp3", "Mp4", "SUPPORTRD_FORMATS"]
