b1cd1dbae88651881e102f8889d3e3f05bb27034
pxeemo <shayans31516@gmail.com>
2024-09-24 00:44:50 +0330

    feat: argparser: added "-y" and "--no-prompt" options

--- lrxy/__main__.py
+++ lrxy/__main__.py
@@ -20,6 +20,18 @@ def main():
         help="path of music file"
     )
 
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-y",
+        action="store_true",
+        help="automatically confirm all prompts"
+    )
+    group.add_argument(
+        "--no-prompt",
+        action="store_true",
+        help="skip all prompts and use default behavior",
+    )
+
     args = parser.parse_args()
 
     files = iter_files(*args.file)
@@ -31,8 +43,39 @@ def main():
             plain_lyric = res["data"]["plainLyrics"]
             synced_lyric = res["data"]["syncedLyrics"]
 
-            audio.embed_lyric(synced_lyric)
-            print("successful embeded synced lyric")
+            if synced_lyric:
+                lyric = synced_lyric
+            elif plain_lyric:
+                if args.y:
+                    print("This song has no synced lyric. " +
+                          "Using plain lyric instead...")
+                    lyric = plain_lyric
+                elif args.no_prompt:
+                    print("This song has no synced lyric. Ignoring...")
+                    continue
+                else:
+                    choice = input(
+                        "This song has no synced lyric. " +
+                        "Use plain lyric? [y/N] ")
+                    if choice.lower() == "y":
+                        lyric = plain_lyric
+                    elif choice.lower() == "n" or not choice:
+                        continue
+                    else:
+                        print("unexpected input")
+                        continue
+            else:
+                print("This song has no lyric.")
+                continue
+
+            if args.separate:
+                lrc_file = audio.path.with_suffix(".lrc")
+                with open(lrc_file, "w", encoding="utf-8") as f:
+                    f.write(lyric)
+                print("successful written lrc file")
+            else:
+                audio.embed_lyric(lyric)
+                print("successful embedded synced lyric")
         else:
             if data == "notfound":
                 print("i can't found lyric for this music")
