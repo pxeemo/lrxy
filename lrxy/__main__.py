ba5f5e3a7c34b1b0ff0fb2c4a5ef4605017119d6
pxeemo <shayans31516@gmail.com>
2025-07-10 15:04:10 +0330

    feat: initial argparse

--- lrxy/__main__.py
+++ lrxy/__main__.py
@@ -8,80 +8,65 @@ def main():
         description="A synced lyric fetcher and embedder for music files"
     )
 
-    parser.add_argument(
-        "-s", "--separate",
-        action="store_true",
-        help="write lyric to a lrc file",
-    )
-
-    parser.add_argument(
-        "file",
-        nargs="+",
-        help="path of music file"
-    )
-
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
-        "-y",
+        "--write-lrc",
         action="store_true",
-        help="automatically confirm all prompts"
+        help="write lyrics to .lrc files"
     )
     group.add_argument(
-        "--no-prompt",
-        action="store_true",
-        help="skip all prompts and use default behavior",
+        "--embed",
+        metavar="LRC_FILE",
+        nargs=1,
+        help="embed existing lyrics file into music"
+    )
+
+    parser.add_argument(
+        "files",
+        metavar="MUSIC_FILE",
+        action="append",
+        nargs="+",
+        help="path of music files to process"
     )
 
     args = parser.parse_args()
+    print(args)
 
-    files = iter_files(*args.file)
-    for res in files:
-        print(f"file: {res['path']}")
-        success, data = res["success"], res["data"]
-        if success:
-            audio = res["path"]
-            plain_lyric = res["data"]["plainLyrics"]
-            synced_lyric = res["data"]["syncedLyrics"]
+    if args.embed:
+        if len(args.files[0]) > 1:
+            parser.error("can't use '--embed' with multiple music files")
+            return
 
-            if synced_lyric:
-                lyric = synced_lyric
-            elif plain_lyric:
-                if args.y:
-                    print("This song has no synced lyric. " +
-                          "Using plain lyric instead...")
-                    lyric = plain_lyric
-                elif args.no_prompt:
-                    print("This song has no synced lyric. Ignoring...")
-                    continue
-                else:
-                    choice = input(
-                        "This song has no synced lyric. " +
-                        "Use plain lyric? [y/N] ")
-                    if choice.lower() == "y":
-                        lyric = plain_lyric
-                    elif choice.lower() == "n" or not choice:
-                        continue
-                    else:
-                        print("unexpected input")
-                        continue
-            else:
-                print("This song has no lyric.")
+        # TODO: read and embed from an lrc file
+        print("--embed is not implemented yet!")
+        return
+
+    for result in iter_files(*args.files[0]):
+        if result['success']:
+            audio = result["music_obj"]
+            plain_lyric = result["data"]["plainLyrics"]
+            synced_lyric = result["data"]["syncedLyrics"]
+            lyric = synced_lyric
+
+            if plain_lyric and not synced_lyric:
+                print("Synced lyric not available. Falling back to plain lyric.")
+                lyric = plain_lyric
+            elif not plain_lyric:
+                print(f"Song has no lyric: {audio.path}")
                 continue
 
-            if args.separate:
+            if args.write_lrc:
                 lrc_file = audio.path.with_suffix(".lrc")
+                if lrc_file.exists():
+                    raise FileExistsError
+
                 with open(lrc_file, "w", encoding="utf-8") as f:
                     f.write(lyric)
-                print("successful written lrc file")
+
+                print(f"Successfully written to: {lrc_file}")
             else:
                 audio.embed_lyric(lyric)
-                print("successful embedded synced lyric")
-        else:
-            if data == "notfound":
-                print("i can't found lyric for this music")
-            else:
-                print(f"error: {data}")
-        print("*"*10)
+                print("Successfully embedded the lyric")
 
 
 if __name__ == "__main__":
