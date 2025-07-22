1862d3e227d4fd381ac3a39e2eab71678c7cd491
MatsubaraCoder <MatsubaraCoder@gmail.com>
2025-07-22 09:34:04 +0330

    change message variable to exception attribute for all exception classes

--- lrxy/exceptions/__init__.py
+++ lrxy/exceptions/__init__.py
@@ -5,27 +5,27 @@ class LrxyException(Exception):
 
 class PathNotExistsError(LrxyException):
     def __init__(self, path: str) -> None:
-        message = f"The path '{path}' does not exist"
-        super().__init__(message)
+        self.message = f"The path '{path}' does not exist"
+        super().__init__(self.message)
 
 
 class FileError(LrxyException):
     def __init__(self, path: str) -> None:
-        message = f"This path '{path}' is not a file"
-        super().__init__(message)
+        self.message = f"This path '{path}' is not a file"
+        super().__init__(self.message)
 
 
 class UnsupportedFileFormatError(LrxyException):
     def __init__(self, unsupported_format: str,
                  supported_formats: list) -> None:
-        message = (
+        self.message = (
             f"Unsupported format '{unsupported_format}'. "
             f"Only supported formats {supported_formats} "
         )
-        super().__init__(message)
+        super().__init__(self.message)
 
 
 class TagError(LrxyException):
     def __init__(self, path: str, tag_name: str) -> None:
-        message = f"This music '{path}' has no tag {tag_name}"
-        super().__init__(message)
+        self.message = f"This music '{path}' has no tag {tag_name}"
+        super().__init__(self.message)
